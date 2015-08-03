81d7447450c984e1af1272d0b6e139b16d805943
audeebudiman <audee.g.b@gmail.com>
2015-08-03 07:09:48 -0700

    master:changing button colour creating separate js file for navigation function

---
+++ app/scripts/directives/navigation.js
@@ -0,0 +1,23 @@
+/**
+ * Created by audee on 8/3/15.
+ */
+
+
+
+$(function() {
+  var pull 		= $('#pull');
+  menu 		= $('nav ul');
+  menuHeight	= menu.height();
+
+  $(pull).on('click', function(e) {
+    e.preventDefault();
+    menu.slideToggle();
+  });
+
+  $(window).resize(function(){
+    var w = $(window).width();
+    if(w > 320 && menu.is(':hidden')) {
+      menu.removeAttr('style');
+    }
+  });
+});
